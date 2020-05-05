f80da9c0fb054e3f72024a3d452ef6b1af4dc7ee
DuGlaser <duglaser0618@gmail.com>
2020-05-05 22:34:10 +0900

    :sparkles:ADD the user patch method

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -5,15 +5,8 @@ module V1
       desc "returns all users"
       get "/" do
         @users = User.all
-      end
-
-      # GET[/:id]
-      desc "returns an User"
-      params do
-        requires :id, type: Integer
-      end
-      get "/:id" do
-        @user = User.find(params[:id])
+        status 201
+        present @users, with: V1::Entities::UserEntity
       end
 
       # POST[/singup]
@@ -29,7 +22,8 @@ module V1
         )
 
         if @user.save
-          @user
+          status 201
+          present @user, with: V1::Entities::UserEntity
         else
           @user.errors.full_messages
         end
@@ -45,9 +39,41 @@ module V1
         @user = User.find_by(email: params[:email])
         if @user.authenticate(params[:password])
           set_cookie(@user)
-          return @user
+          status 201
+          present @user, with: V1::Entities::UserEntity
         else
           error!("Unauthorized. Invalid email or password.", 401)
+        end
+      end
+
+      # GET[/:id]
+      desc "returns an User"
+      params do
+        requires :id, type: Integer
+      end
+      get "/:id" do
+        @user = User.find(params[:id])
+        status 201
+        present @user, with: V1::Entities::UserEntity
+      end
+
+      # patch[/:id]
+      desc "Delete an User"
+      params do
+        requires :name, type: String
+        requires :bio, type: String
+      end
+      patch "/:id" do
+        @user = User.find_by("id": params[:id])
+        if @user.authenticated?(cookies[:remember_token])
+          @user.name = params[:name] if params[:content].present?
+          @user.bio = params[:bio] if params[:bio].present?
+          if (@user.save)
+            status 201
+            present @user, with: V1::Entities::UserEntity
+          end
+        else
+          error!("permission denied", 401)
         end
       end
 
@@ -57,10 +83,12 @@ module V1
         requires :id, type: Integer
       end
       delete "/:id" do
-        @user = User.find(cookies[:user_id])
-        if @user.authenticated?(cookies[:remember_token]) && params[:id] == cookies[:user_id]
+        @user = User.find_by("id": params[:id])
+        if @user.authenticated?(cookies[:remember_token])
           @user = User.find(cookies[:id])
           @user.destroy
+          status 201
+          present @user, with: V1::Entities::UserEntity
         else
           error!("permission denied", 401)
         end
