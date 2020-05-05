d0a25b28ab5b87575a8beb9291b865691b0209c3
DuGlaser <duglaser0618@gmail.com>
2020-05-05 15:09:33 +0900

    :sparkles:ADD the bio to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_014912) do
+ActiveRecord::Schema.define(version: 2020_05_05_050334) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "bio"
   end
 
 end
