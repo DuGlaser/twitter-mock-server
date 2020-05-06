41bfbbbc9edffa864b1ce84f2e5c120d8569b410
DuGlaser <duglaser0618@gmail.com>
2020-05-06 17:55:59 +0900

    :sparkles:ADD the relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   has_secure_password
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_many :tweets, dependent: :destroy
 
@@ -35,5 +43,17 @@ class User < ApplicationRecord
     else
       return false
     end
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
