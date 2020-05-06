25df0d04f17e0956dbd0c22c01be1e3c4d3e5641
DuGlaser <duglaser0618@gmail.com>
2020-05-06 10:52:09 +0900

    :sparkles:ADD the tweet model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   has_secure_password
 
+  has_many :tweets
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
